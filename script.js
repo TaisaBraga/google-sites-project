f7d1dcadfb8ef13989bc35a34f2b98a0ee939b36
Taisa Braga <taisabraga.adm@gmail.com>
2022-04-23 22:41:01 -0300

    add au pair topic

--- script.js
+++ script.js
@@ -10,9 +10,14 @@ const titleThree = document.getElementById('titleThree')
 const textThree = document.getElementById('textThree')
 const iconThree = document.querySelector('#iconThree')
 
+const titleFour = document.getElementById('titleFour')
+const textFour = document.getElementById('textFour')
+const iconFour = document.querySelector('#iconFour')
+
 let isVisible = false
 let isVisibleTwo = false
 let isVisibleThree = false
+let isVisibleFour = false
 
 title.onclick = () => {
   textOff.style.display = isVisible ? 'block' : 'none'
@@ -30,4 +35,10 @@ titleThree.onclick = () => {
   textThree.style.display = isVisibleThree ? 'block' : 'none'
   iconThree.style.transform = isVisibleThree ? 'rotate(180deg)' : 'rotate(0deg)'
   isVisibleThree = !isVisibleThree
+}
+
+titleFour.onclick = () => {
+  textFour.style.display = isVisibleFour ? 'block' : 'none'
+  iconFour.style.transform = isVisibleFour ? 'rotate(180deg)' : 'rotate(0deg)'
+  isVisibleFour = !isVisibleFour
 }

39bfae344003244897b7e21770b535586f70afc9
Taisa Braga <taisabraga.adm@gmail.com>
2022-04-23 22:14:59 -0300

    Add topic study and work

--- script.js
+++ script.js
@@ -2,16 +2,21 @@ const title = document.getElementById('title')
 const textOff = document.getElementById('textOff')
 const icon = document.querySelector('#icon')
 
-let isVisible = true
+const titleTwo = document.getElementById('titleTwo')
+const textTwo = document.getElementById('textTwo')
+const iconTwo = document.querySelector('#iconTwo')
+
+let isVisible = false
+let isVisibleTwo = false
 
 title.onclick = () => {
-  if (isVisible) {
-    textOff.style.display = 'block'
-    icon.style.transform = 'rotate(0deg)'
-    isVisible = false
-  } else {
-    textOff.style.display = 'none'
-    icon.style.transform = 'rotate(180deg)'
-    isVisible = true
-  }
+  textOff.style.display = isVisible ? 'block' : 'none'
+  icon.style.transform = isVisible ? 'rotate(180deg)' : 'rotate(0deg)'
+  isVisible = !isVisible
+}
+
+titleTwo.onclick = () => {
+  textTwo.style.display = isVisibleTwo ? 'block' : 'none'
+  iconTwo.style.transform = isVisibleTwo ? 'rotate(180deg)' : 'rotate(0deg)'
+  isVisibleTwo = !isVisibleTwo
 }

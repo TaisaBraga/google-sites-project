258df14653863eeb8dfa12938764715be6ae18f6
Taisa Braga <taisabraga.adm@gmail.com>
2022-04-23 22:23:35 -0300

    Add volunteer topic

--- script.js
+++ script.js
@@ -6,8 +6,13 @@ const titleTwo = document.getElementById('titleTwo')
 const textTwo = document.getElementById('textTwo')
 const iconTwo = document.querySelector('#iconTwo')
 
+const titleThree = document.getElementById('titleThree')
+const textThree = document.getElementById('textThree')
+const iconThree = document.querySelector('#iconThree')
+
 let isVisible = false
 let isVisibleTwo = false
+let isVisibleThree = false
 
 title.onclick = () => {
   textOff.style.display = isVisible ? 'block' : 'none'
@@ -19,4 +24,10 @@ titleTwo.onclick = () => {
   textTwo.style.display = isVisibleTwo ? 'block' : 'none'
   iconTwo.style.transform = isVisibleTwo ? 'rotate(180deg)' : 'rotate(0deg)'
   isVisibleTwo = !isVisibleTwo
+}
+
+titleThree.onclick = () => {
+  textThree.style.display = isVisibleThree ? 'block' : 'none'
+  iconThree.style.transform = isVisibleThree ? 'rotate(180deg)' : 'rotate(0deg)'
+  isVisibleThree = !isVisibleThree
 }
